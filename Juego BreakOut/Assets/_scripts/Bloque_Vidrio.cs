f35bb4d2b197783145e5326ac471db43057648a2
SEBAS-111 <sebasholalego587@gmail.com>
2025-10-29 01:35:25 -0600

    modificacion
    -modificación al bloque vidrio

--- Juego BreakOut/Assets/_scripts/Bloque_Vidrio.cs
+++ Juego BreakOut/Assets/_scripts/Bloque_Vidrio.cs
@@ -5,7 +5,7 @@ public class Bloque_Vidrio : Bloque
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        resistencia = 3;
+        resistencia = 2;
     }
 
 
