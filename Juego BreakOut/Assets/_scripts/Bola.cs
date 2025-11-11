066e4a4352738c1b8b6697de9caa75b79bd6e0c7
SEBAS-111 <sebasholalego587@gmail.com>
2025-11-10 19:33:04 -0600

    modificacion
    -recuperando lo perdido

--- Juego BreakOut/Assets/_scripts/Bola.cs
+++ Juego BreakOut/Assets/_scripts/Bola.cs
@@ -1,16 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class bola : MonoBehaviour
 {
+
+    public bool isGameStarted = false;
+    [SerializeField] public float velocidadBola = 10.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Vector3 posicionInicial = GameObject.FindGameObjectWithTag("Jugador").transform.position;
+        posicionInicial.y += 3;
+        this.transform.position = posicionInicial;
+        this.transform.SetParent(GameObject.FindGameObjectWithTag("Jugador").transform);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Space) || Input.GetButton("Submit"))
+        {
+            if (!isGameStarted)
+            {
+                isGameStarted = true;
+                this.transform.SetParent(null);
+                GetComponent<Rigidbody>().linearVelocity = velocidadBola * Vector3.up;
+            }
+        }
     }
 }
--- Juego BreakOut/Assets/_scripts/Jugador.cs
+++ Juego BreakOut/Assets/_scripts/Jugador.cs
@@ -1,16 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class jugador : MonoBehaviour
 {
+    [SerializeField] public int limiteX = 23;
+    [SerializeField] public float velocidadPaddle = 0.9f;
+
+    Transform Transform;
+    Vector3 mousePos2D;
+    Vector3 mousePos3D;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Transform = this.gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //mousePos2D = Input.mousePosition;
+        //mousePos2D.z = -Camera.main.transform.position.z;
+        //mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    this.transform.Translate(Vector3.down * velocidadPaddle * Time.deltaTime);
+        //}
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    this.transform.Translate(Vector3.up * velocidadPaddle * Time.deltaTime);
+        //}
+
+        transform.Translate(Input.GetAxis("Horizontal") * Vector3.down * velocidadPaddle * Time.deltaTime);
+
+        Vector3 pos = transform.position;
+        //pos.x = mousePos3D.x;
+        if (pos.x < -limiteX)
+        {
+            pos.x = -limiteX;
+        }
+        else if (pos.x > limiteX)
+        {
+            pos.x = limiteX;
+        }
+        this.transform.position = pos;
+
     }
 }
